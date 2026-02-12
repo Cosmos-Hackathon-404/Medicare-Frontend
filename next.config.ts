67204459e59ff3e3baa9d30eea0204c5b8689487
Abinash Yadav <abinashyadav3.141@gmail.com>
2026-02-12 19:49:46 +0545

    latest

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   typescript: {
     ignoreBuildErrors: true,
-=======
+  },
   images: {
     remotePatterns: [
       {
@@ -12,7 +11,6 @@ const nextConfig: NextConfig = {
         hostname: 'illustrations.popsy.co',
       },
     ],
->>>>>>> 606e68f70a8c3c55a6a94a7e490ec2caca1df0c9
   },
 };
 
